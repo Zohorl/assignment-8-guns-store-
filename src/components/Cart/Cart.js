95e06780a55b627e3ec744c3709a58bea0574a3d
Zohorl <zohorul109561@gmail.com>
2022-03-26 12:35:39 +0600

    cart some guns added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './Cart.css'
+const Cart = ({ cart }) => {
+    // console.log(cart)
+    let total = 0;
+    for (const gun of cart) {
+        // console.log(gun);
+        total = total + parseFloat(gun.price);
+        const x = gun.name;
 
-const Cart = () => {
+    }
     return (
-        <div>
-            <h4>hello world !!!!!!!!!!!</h4>
+        <div className='cart'>
+
+            <h5>price : {total}</h5>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Cart from '../Cart/Cart';
 import Guns from '../Guns/Guns';
 import './Shop.css';
 const Shop = () => {
@@ -28,7 +29,7 @@ const Shop = () => {
                     }
                 </div>
                 <div className="cart-container">
-
+                    <Cart cart={cart}></Cart>
                 </div>
             </div>
         </div>
