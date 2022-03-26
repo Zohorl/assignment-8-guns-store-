5e7783879cb332be68252c791d6b5fb30b04ae4c
Zohorl <zohorul109561@gmail.com>
2022-03-26 07:59:26 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Guns from './components/Guns/Guns';
+
+function App() {
+  return (
+    <div className="App">
+      <Guns></Guns>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Guns/Guns.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Guns = () => {
+    return (
+        <div>
+            <h1>Guns store</h1>
+        </div>
+    );
+};
+
+export default Guns;
